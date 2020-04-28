41bccd57568cf1b9d5b03d24bb7038aae34f5967
Hyrwing Souza <hyrwing.souza@exxonmobil.com>
2020-04-28 18:33:07 -0300

    Updates

---
+++ precache-manifest.ce6f1c48d768e26a5f4d3edb82d5a1de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b631a6fddce052ef3b65dfa871a339b3",
+    "url": "/colab-cards/index.html"
+  },
+  {
+    "revision": "49052546117c685100e1",
+    "url": "/colab-cards/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "64c6387c7fc7850ff5a4",
+    "url": "/colab-cards/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "49052546117c685100e1",
+    "url": "/colab-cards/static/js/2.2da2d652.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/colab-cards/static/js/2.2da2d652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64c6387c7fc7850ff5a4",
+    "url": "/colab-cards/static/js/main.031c3e1a.chunk.js"
+  },
+  {
+    "revision": "55be35660696cce55518",
+    "url": "/colab-cards/static/js/runtime-main.e40bf93a.js"
+  }
+]);
