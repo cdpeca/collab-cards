8cc543b4edbb581beb30db372e4f6a8921ea3a85
Eduardo Reche <edu.reche@gmail.com>
2020-04-28 16:03:58 -0300

    feat(model): set card state

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
 import './App.css';
 import Card from './components/Card';
 import Arrow from './components/Arrow';
 
 function App() {
+  const [current, setCurrent] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <Arrow direction="up" />
-        <Card
-          title="Test"
-          subtitle="Sub"
-          image="Image test"
-        />
-      </header>
+      <Container>
+        <Row>
+          <Col>
+            <Arrow direction="left" />
+          </Col>
+          
+          <Col>
+            <Card
+              title="Test"
+              subtitle="Sub"
+              image="Image test"
+            />
+          </Col>
+          
+          <Col>
+            <Arrow direction="right" />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
